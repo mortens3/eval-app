ea7612d4fd1c2b0b5acef4185efd084e483cb710
Morten Schioldan <mortens@microsoft.com>
2017-05-30 20:11:39 +0200

    dockerized

---
+++ eval-web/data-access/index.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+var request = require('request');
+
+var contentApiUrl = 'http://localhost:8081';
+
+function getSessions(cb) {
+    request(contentApiUrl + '/bears', function (err, response, body) {
+        if (err) {
+            return cb(err);
+        }
+        var data = JSON.parse(body);
+        var apiInstance = response.headers["x-mesos-taskid"];
+        cb(null, data, apiInstance);
+    });
+}
+
+module.exports = {
+    getBears: getBears
+};
--- eval-web/server.js
+++ eval-web/server.js
@@ -6,22 +6,31 @@ var querystring = require('querystring');
 var https = require('https');
 var request = require('request');
 
-
-
 // Constants
 const PORT = 8080;
 
 // App
 var app = express();
+var dataAccess = require('./data-access/index');
 
 app.set('view engine', 'ejs');
-
-
 
 // index page 
 app.get('/', function (req, res) {
     res.render('index');
 });
+
+app.get('/api/bears', function (req, res) {
+    dataAccess.getBears(function (error, data, apiInstance) {
+        if (error) {
+            return res.status(500).send(error);
+        }
+        else {
+            return res.status(200).send(data);
+        }
+    });
+});
+
 
 
 app.use(express.static('public'));
